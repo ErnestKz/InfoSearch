bbfce6a05b5b24041e63b230393d4d140c6f5496
ernestkz <ernestkuznecovs@gmail.com>
2021-10-15 01:16:38 +0100

    tidy up

--- Assignment1/src/main/java/Part1.java
+++ Assignment1/src/main/java/Part1.java
@@ -49,49 +49,44 @@ public class Part1 {
 
     private static int NUM_QUERIES = 225;
     private static int NUM_DOCUMENTS = 1400;
+    
     private static String INDEX_DOC_ID_FIELD = "id";
     private static String INDEX_DOC_CONTENT_FIELD = "content";
     
     public static void main(String[] args) throws IOException {
 	// Analyzer analyzer = new StandardAnalyzer();
 	Analyzer analyzer = new EnglishAnalyzer();
-	// Similarity similarity = new BM25Similarity(2.4f, 0.8f);
-	Similarity similarity = new BM25Similarity(0.8f, 0.2f);
+	Similarity similarity = new BM25Similarity(2.8f, 0.85f);
+	// Similarity similarity = new BM25Similarity(0.8f, 0.2f);
 	// Similarity similarity = new BM25Similarity();
 	// Similarity similarity = new ClassicSimilarity();
 
+	Directory indexDirectory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
+
 	long start = System.currentTimeMillis();
-	
+	System.out.print((System.currentTimeMillis() - start) / 10);
+	System.out.println(" start of timer.");
+
+	// Read corpus files and write to index.
 	IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
 	indexWriterConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
 	indexWriterConfig.setSimilarity(similarity);
-
-	System.out.print((System.currentTimeMillis() - start) / 10);
-	System.out.println(" created indexWriter config.");
-
-	Directory indexDirectory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
-
 	IndexWriter indexWriter = new IndexWriter(indexDirectory, indexWriterConfig);
 	List<Document> corpusDocuments = readCorpusAndBuildDocuments();
 	indexWriter.addDocuments(corpusDocuments);
 	indexWriter.close();
-
+	
 	System.out.print((System.currentTimeMillis() - start) / 10);
-	System.out.println(" read and wrote indexed corpus.");
+	System.out.println(" read and wrote index of corpus.");
 
-	DirectoryReader indexDirectoryReader = DirectoryReader.open(indexDirectory);
-	IndexSearcher indexSearcher = new IndexSearcher(indexDirectoryReader);
-	indexSearcher.setSimilarity(similarity);
-	
+	// Read queries from file, analyse them, construct query object.
 	List<Pair<String, String>> queriesIdAndContent = loadQueriesIdAndContent();
-	
 	List<Pair<String, List<String>>> queriesIdAndTokens =
 	    queriesIdAndContent.stream()
 	    .map(idContentPair ->
 		 Pair.of(idContentPair.getLeft(),
 			 queryStringToTokens(idContentPair.getRight(), analyzer)))
 	    .collect(toList());
-	
 	List<Pair<String, Query>> queriesIdAndQuery =
 	    queriesIdAndTokens.stream()
 	    .map(idTokensPair ->
@@ -100,17 +95,24 @@ public class Part1 {
 
 	System.out.print((System.currentTimeMillis() - start) / 10);
 	System.out.println(" read, analysed, and constructed queries.");
-	    
+
+	
+	DirectoryReader indexDirectoryReader = DirectoryReader.open(indexDirectory);
+	IndexSearcher indexSearcher = new IndexSearcher(indexDirectoryReader);
+	indexSearcher.setSimilarity(similarity);
+	
+	// Execute queries.
 	List<Pair<String, TopDocs>> queriesIdAndSearchResult =
 	    queriesIdAndQuery.stream()
 	    .map(idQueryPair ->
 		 Pair.of(idQueryPair.getLeft(),
 			 queryToSearchResult(idQueryPair.getRight(), indexSearcher)))
 	    .collect(toList());
-
+	
 	System.out.print((System.currentTimeMillis() - start) / 10);
 	System.out.println(" executed "+ NUM_QUERIES + " queries.");
 
+	// Build result string for trec for each query.
 	List<Pair<String, String>> queriesIdAndResultString =
 	    queriesIdAndSearchResult.stream()
 	    .map(idTopDocsPair ->
@@ -123,30 +125,21 @@ public class Part1 {
 	System.out.print((System.currentTimeMillis() - start) / 10);
 	System.out.println(" constructed results string from "+ NUM_QUERIES + " queries, with " + NUM_DOCUMENTS + " documents retrieved each.");
 	
+	indexDirectory.close();
+	indexDirectoryReader.close();
+
+
+	// Concatenate result strings and write to file.
 	String resultString = queriesIdAndResultString.stream()
 	    .reduce("", (subtotal, queryIdAndResultString) ->
 		    subtotal + (queryIdAndResultString.getRight()),
 		    String::concat);
-
-	indexDirectory.close();
-	indexDirectoryReader.close();
 
 	File queryResultsFile = new File(QUERY_RESULTS_FILE);
 	queryResultsFile.createNewFile();
 	try(FileWriter queryResultFileWriter = new FileWriter(queryResultsFile)) {
 	    queryResultFileWriter.write(resultString);
 	}
-
-	// String queryString1 = queriesIdAndContent.get(0).getRight();
-	// List<String> queryTokens1 = queryStringToTokens(queryString1, analyzer);
-	// queryTokens1.stream()
-	//     .forEach(s -> System.out.println(s));
-
-	// System.out.println("\n");
-	// System.out.print(readAllContents(QUERY_DIRECTORY + "001"));
-
-	// loadQueriesExecuteCreateResultsFile(analyzer, similarity);
-    
     }
 
     private static ArrayList<Document> readCorpusAndBuildDocuments() throws IOException {
@@ -216,7 +209,7 @@ public class Part1 {
 
     private static
 	TopDocs queryToSearchResult(Query query,
-				    IndexSearcher indexSearcher) {
+				    IndexSearcher indexSearcher ) {
 	TopDocs queryResult = null;
 	try { queryResult = indexSearcher.search(query, NUM_DOCUMENTS); }
 	catch (IOException e) {
@@ -229,8 +222,7 @@ public class Part1 {
     private static
 	String searchResultToResultString(TopDocs topDocs,
 						 String queryId,
-						 IndexSearcher indexSearcher
-						 ) {
+						 IndexSearcher indexSearcher ) {
 	StringBuilder resultString = new StringBuilder();
 	try {
 	    ScoreDoc[] documentScores = topDocs.scoreDocs;
@@ -255,7 +247,7 @@ public class Part1 {
     private static
 	String resultLine(String queryId,
 			  String documentId,
-			  Float documentScore) {
+			  Float documentScore ) {
 	// trec_eval solely uses the score to sort the documents to determine the rank.
 	return(queryId    +
 	       " Q0 "     +
@@ -269,98 +261,5 @@ public class Part1 {
 	String readAllContents( String filePath ) throws IOException {
 	return new String(Files.readAllBytes(Paths.get(filePath))); 
     }
-    
 
-
-
-
-
-
-
-
-
-
-    private static
-	void loadQueriesExecuteCreateResultsFile ( Analyzer   analyzer,
-						   Similarity similarity
-						   ) throws IOException {
-
-	StringBuilder resultString = new StringBuilder();
-	String[]      queryFileNames;
-	IndexSearcher indexSearcher;
-	
-	try (Directory queryDirectory = FSDirectory.open(Paths.get(QUERY_DIRECTORY));
-	     Directory indexDirectory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
-	     DirectoryReader indexDirectoryReader = DirectoryReader.open(indexDirectory)) {
-	    
-	    indexSearcher = new IndexSearcher(indexDirectoryReader);
-	    indexSearcher.setSimilarity(similarity);
-
-	    queryFileNames = queryDirectory.listAll();
-	    
-	    for (int i = 0; i < queryFileNames.length; i++) {
-		String queryFileName = queryFileNames[i];
-		String queryString   = readAllContents(QUERY_DIRECTORY + queryFileName);
-		String queryId       = (i + 1) + "";
-		resultString.append(runQueryAndGetDocumentScores(analyzer,
-								 queryString,
-								 queryId,
-								 indexSearcher));
-	    }
-	}
-
-	File queryResultsFile = new File(QUERY_RESULTS_FILE);
-	queryResultsFile.createNewFile();
-	
-	try(FileWriter queryResultFileWriter = new FileWriter(queryResultsFile)) {
-	    queryResultFileWriter.write(resultString.toString());
-	}
-    }
-
-    private static
-	StringBuilder runQueryAndGetDocumentScores ( Analyzer   analyzer,
-						     String     queryString,
-						     String     queryId,
-						     IndexSearcher indexSearcher
-						     ) throws IOException {
-	
-	// TODO Kind of want to know about the what indexSearcher.search() returns.
-	// Maybe I can do some nice functional style stuff.
-	
-	Query booleanQuery = buildQuery(queryString, analyzer);
-	ScoreDoc[] documentScores = indexSearcher
-	    .search(booleanQuery, NUM_QUERIES)
-	    .scoreDocs;
-
-	StringBuilder resultString = new StringBuilder();
-	
-	for (int i = 0; i < documentScores.length; i++) {
-	    Document hitDoc = indexSearcher.doc(documentScores[i].doc);
-	    String documentId    = hitDoc.get(INDEX_DOC_ID_FIELD);
-	    Float  documentScore = documentScores[i].score;
-	    
-	    resultString.append(resultLine(queryId,
-					   documentId,
-					   documentScore));
-	}
-	
-	return resultString;
-    }
-
-    private static
-	Query buildQuery(String queryString,
-			 Analyzer analyzer) throws IOException {
-	// TODO Need to know about how token streams work
-	// Wondering if the below mess is necessary.
-	BooleanQuery.Builder booleanQueryBuilder = new BooleanQuery.Builder();
-	TokenStream tokenStream = analyzer.tokenStream(INDEX_DOC_CONTENT_FIELD, queryString);
-	CharTermAttribute attr = tokenStream.addAttribute(CharTermAttribute.class);
-	tokenStream.reset();
-	while(tokenStream.incrementToken()) {
-	    Query term = new TermQuery(new Term(INDEX_DOC_CONTENT_FIELD, attr.toString()));
-	    booleanQueryBuilder.add(new BooleanClause(term, BooleanClause.Occur.SHOULD));
-	}       
-	tokenStream.close();
-	return booleanQueryBuilder.build();
-    }
 }
